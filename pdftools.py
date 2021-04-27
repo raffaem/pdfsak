fb45b58a60e4c033ccea9a3b0c0a3b9a35d59ca6
Raffaele Mancuso <yuilytg7@gmail.com>
2021-04-28 00:42:32 +0200

    Re-licensed under more permissive MIT license

--- pdftools.py
+++ pdftools.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python3
-
 # coding=utf8
-#
-#   pdftools - manipulate pdf files
-#   Copyright (C) 2021 Raffaele Mancuso
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Copyright (c) 2021 Raffaele Mancuso
 
 import sys
 import zipfile  # Used to compress debug files togheter to be sent for analysis
