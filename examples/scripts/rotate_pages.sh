76b796d0460d27b6a6da1f873ac80e0e54b73a0b
Raffaele Mancuso <yuilytg7@gmail.com>
2021-09-27 11:49:10 +0200

    Fix Rotate pages example

--- examples/scripts/rotate_pages.sh
+++ examples/scripts/rotate_pages.sh
@@ -2,4 +2,4 @@
 
 # This example swap the pages of a input PDF file
 
-../pdftools.py --input-file ./input/numbered.pdf --output ./output/rotate_pages.pdf --overwrite --rotate-pages "1=90;2=180;4=270" 
+../pdftools.py --input-file ../input/numbered.pdf --output ../output/rotate_pages.pdf --overwrite --rotate-pages "1=90;2=180;4=270" 
