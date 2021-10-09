d48870e4d4117484dd05b3b07a1c597c41165662
Raffaele Mancuso <yuilytg7@gmail.com>
2021-10-09 19:21:30 +0200

    Fix example of extract_pages

--- examples/scripts/extract_pages.sh
+++ examples/scripts/extract_pages.sh
@@ -2,4 +2,4 @@
 
 # This example extract the pages of a input PDF file
 
-../../pdftools.py --input-file ../input/numbered.pdf --output ../output/extract_pages.pdf --pages "1,3,20-"
+../../pdftools.py --input-file ../input/numbered.pdf --output ../output/extract_pages.pdf --extract-pages "1,3,20-"
