fdcc0b977f74bbd80228fe654c71e65f20e77b50
Raffaele Mancuso <yuilytg7@gmail.com>
2021-10-10 22:01:02 +0200

    Files for setuptools build

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(name='pdftools',
-      version='0.1',
-      description='Utility to manage PDF files',
-      author='Raffaele Mancuso',
-      author_email='raffaelemancuso532@gmail.com',
-      url='https://github.com/raffaem/pdftools',
-      scripts=['./pdftools']
-     )
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pdftools",
+    version="1.0",
+    author="Raffaele Mancuso",
+    author_email="raffaelemancuso532@gmail.com",
+    description="Utility to manipulate PDF files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raffaem/pdftools",
+    project_urls={
+        "Bug Tracker": "https://github.com/raffaem/pdftools/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['./pdftools'],
+    python_requires=">=3.6",
+)
+
