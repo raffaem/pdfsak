262459964ef605578927460005f58683c59e6d02
Raffaele Mancuso <yuilytg7@gmail.com>
2021-10-10 14:18:55 +0200

    Setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pdftools',
+      version='0.1',
+      description='Utility to manage PDF files',
+      author='Raffaele Mancuso',
+      author_email='raffaelemancuso532@gmail.com',
+      url='https://github.com/raffaem/pdftools',
+      scripts=['./pdftools']
+     )
