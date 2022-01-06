99d4956303aace4b465f713373a27d8aeed4d928
Raffaele Mancuso <yuilytg7@gmail.com>
2022-01-06 20:19:53 +0100

    Use copy instead of cp in Windows example

--- examples/batch/scripts/replace_input.bat
+++ examples/batch/scripts/replace_input.bat
@@ -1,7 +1,7 @@
 @echo off
 REM This example replaces the input PDF file instead of writing to a separate output file.
 
-cp ..\..\input\numbered.pdf ..\..\output\replace_input.pdf
+copy ..\..\input\numbered.pdf ..\..\output\replace_input.pdf
 
 python ..\..\..\pdfsak --input-file ..\..\output\replace_input.pdf --replace-input --extract-pages "1"
 
