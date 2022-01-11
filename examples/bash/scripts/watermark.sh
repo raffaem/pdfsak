577a91d974b07a656f67131f160b179bf08e72a9
Raffaele Mancuso <yuilytg7@gmail.com>
2022-01-11 22:35:53 +0100

    Adjust for width of watermark

--- examples/bash/scripts/watermark.sh
+++ examples/bash/scripts/watermark.sh
@@ -2,4 +2,4 @@
 
 # This example adds a watermark
 
-../../../pdfsak --input-file ../../input/presentation.pdf --output ../../output/watermark.pdf --watermark ../../input/tux.png cm 0.5 0.5 0.3 0.5 --overwrite --textpos-showboxes
+../../../pdfsak --input-file ../../input/presentation.pdf --output ../../output/watermark.pdf --watermark ../../input/tux.png cm 0.5 0.5 0.2 0.5 --overwrite
