e97a4d867e0f09005f32884d6418e4d013dff5a6
Raffaele Mancuso <yuilytg7@gmail.com>
2022-01-12 07:46:17 +0100

    Watermark example for Windows

---
+++ examples/batch/scripts/watermark.bat
@@ -0,0 +1,3 @@
+REM This example adds a watermark
+
+python ..\..\..\pdfsak --input-file ..\..\input\presentation.pdf --output ..\..\output\watermark.pdf --watermark ..\..\input\tux.png cm 0.5 0.5 0.2 0.5 --overwrite
