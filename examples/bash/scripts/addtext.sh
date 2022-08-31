83b7617c8450bf7281d1137afb42c5aba39f82c7
Raffaele Mancuso <54762742+raffaem@users.noreply.github.com>
2022-08-31 13:44:27 +0200

    Bump version to 1.3.1

--- examples/bash/scripts/addtext.sh
+++ examples/bash/scripts/addtext.sh
@@ -4,4 +4,5 @@
 # - Page numbers in the bottom right corner (in the format `current_page/total_pages`)
 # - Current date in the top left corner (in the format `day/month/year`)
 
-../../../pdfsak --input-file ../../input/article1.pdf --output ../../output/addtext.pdf --text "\huge \$page/\$pages" br 1 1 --text "\huge \$day/\$month/\$year" tl 0 0
+../../../pdfsak --input-file ../../input/article1.pdf --output ../../output/addtext.pdf --text "\huge \$page/\$pages" br 1 1 --text "\huge \$day/\$month/\$year" tl 0 0 --overwrite
+
