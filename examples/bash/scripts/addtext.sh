c086e5aea9e267db46f7fc44babe0c26cdedba73
Raffaele Mancuso <raffaele.mancuso4@unibo.it>
2023-08-30 06:52:56 +0200

    Add --font option

--- examples/bash/scripts/addtext.sh
+++ examples/bash/scripts/addtext.sh
@@ -4,5 +4,5 @@
 # - Page numbers in the bottom right corner (in the format `current_page/total_pages`)
 # - Current date in the top left corner (in the format `day/month/year`)
 
-../../../pdfsak --input-file ../../input/article1.pdf --output ../../output/addtext.pdf --text "\huge \$page/\$pages" br 1 1 --text "\huge \$day/\$month/\$year" tl 0 0 --overwrite
+../../../pdfsak --input-file ../../input/article1.pdf --output ../../output/addtext.pdf --font "EB Garamond" --text "\huge \$page/\$pages" br 1 1 --text "\huge \$day/\$month/\$year" tl 0 0 --overwrite --latex-engine xelatex
 
